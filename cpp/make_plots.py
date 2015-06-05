9f37f34ff743aa3539c33fcbe99a94ef95936834
Jacob Steinhardt <jsteinhardt@cs.stanford.edu>
2015-06-05 14:35:43 -0700

    make_plots

--- cpp/make_plots.py
+++ cpp/make_plots.py
@@ -4,15 +4,24 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
+#outputdir = "/afs/cs.stanford.edu/u/jsteinhardt/NLP-HOME/scr/relaxed-supervision/output"
+#outputdir = "bvsN"
+outputdir = "output5"
+#outputdir = "output4"
+
 import fileinput
 args = []
 #opts = ['trace', 'trace2', 'beta']
+#opts = ['avg_samples']
 opts = ['trace2']
-agg_opts = [('beta_val', 'tied_beta', 'N')]
+agg_opts = [] #[('beta_val', 'tied_beta', 'N')]
 agg = [defaultdict(list) for _ in agg_opts]
 descs = defaultdict(dict)
 for line in fileinput.input():
   toks = line.rstrip("\n").split()
+  if toks[0] == 'dir':
+    outputdir = toks[1]
+    continue
   if toks[0] == 'desc':
     order = int(toks[1])
     name = toks[2]
@@ -28,8 +37,6 @@ for line in fileinput.input():
 print 'args', args
 matches = [[] for _ in args]
 
-#outputdir = "/afs/cs.stanford.edu/u/jsteinhardt/NLP-HOME/scr/relaxed-supervision/output"
-outputdir = "output5"
 from os import listdir
 from os.path import isfile, join
 from itertools import islice
@@ -89,6 +96,9 @@ for arg, match in zip(args, matches):
     for line in f:
       toks = line.rstrip("\n").split()
       if len(toks) == 0:
+        prev_beta = "BETA:" in toks
+        prev_theta_diag = "THETA_diag:" in toks
+        prev_freqs = "FREQS:" in toks
         continue
       if "iteration" in toks:
         if iteration:
@@ -120,12 +130,15 @@ for arg, match in zip(args, matches):
       prev_freqs = "FREQS:" in toks
     print iterations
     if 'trace' in opts:
-      plt.plot([it['trace']/102.0 for it in iterations if 'trace' in it], '%s-o' % colors[ii % len(colors)]) #, label='trace (%s)' % name)
+      V = full_arg['W']
+      plt.plot([it['trace']/float(V) for it in iterations if 'trace' in it], '%s-o' % colors[ii % len(colors)]) #, label='trace (%s)' % name)
     if 'trace2' in opts:
-      line, = plt.plot([it['trace2']/102.0 for it in iterations if 'trace2' in it], '.-', color=colors[order % len(colors)]) #, label='%s' % name)
+      V = full_arg['W']
+      line, = plt.plot([it['trace2']/float(V) for it in iterations if 'trace2' in it], '.-', color=colors[order % len(colors)]) #, label='%s' % name)
       handles_dict[order] = (line, name)
     if 'avg_samples' in opts:
-      plt.plot([it['avg_samples']/100.0 for it in iterations if 'avg_samples' in it], '%s-s' % colors[order % len(colors)], label='samples/100 (%s)' % name)
+      line, = plt.semilogy([it['avg_samples'] for it in iterations if 'avg_samples' in it], '%s-s' % colors[order % len(colors)], label='samples/100 (%s)' % name)
+      handles_dict[order] = (line, name)
     if 'beta' in opts and 'a0' in name and 't0' in name:
       plt.figure(2)
       f, axarr = plt.subplots(2)
@@ -145,10 +158,16 @@ for arg, match in zip(args, matches):
   handles_list = [h[1][0] for h in sorted(handles_dict.items())]
   labels_list = [h[1][1] for h in sorted(handles_dict.items())]
   print handles_list
-  plt.legend(handles_list, labels_list, loc=4,prop={'size':10})
+  plt.legend(handles_list, labels_list, loc=4,prop={'size':17})
   #plt.title(desc)
-  plt.xlabel('iterations')
-  plt.ylabel('accuracy')
+  plt.xlabel('iteration', fontsize=24)
+  plt.ylabel('accuracy', fontsize=24)
+  plt.xlim([0,50])
+  if 'trace2' in opts:
+    plt.ylim([0,1])
+  plt.tick_params(axis='x', labelsize=20)
+  plt.tick_params(axis='y', labelsize=20)
+  plt.tight_layout()
   plt.savefig('%s.pdf' % desc)
 
   # handle agg_opts
